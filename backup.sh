a44f80124341d2c3b0a6d37fb74df6806a0699d8
Vignesh Gurusamy <vigneshgurusamy@users.noreply.github.com>
2013-12-30 22:25:49 +0530

    Added provision to backup multiple databases

--- backup.sh
+++ backup.sh
@@ -2,47 +2,64 @@
 ################################################################################
 # Date    : 2013/12/12
 # Author  : Vignesh Gurusamy
-# Program : Moodle backup script
+# Program : MySQL backup script
 ################################################################################
 
+# Declare the variable 
+# Please add a new vaiable for additional database
+# Associative array will work only after bash version 4
+declare -A db1 db2
+
 # MySQL Variable
-MYSQL_HOST_NAME="localhost"
-MYSQL_USER_NAME="root"
-MYSQL_USER_PWD=""
-MYSQL_DB_NAME="test"
+# MySQL user should have permission to access the database
+# Create an array like this for adding a new database
+# Example :
+# db1=(["hostname"]="localhost" ["username"]="root" ["password"]="" ["dbname"]="test" ["siteurl"]="xyz.com")
 
-# Backup variable
-HN=`hostname | awk -F. '{print $1}'`
-BK_PATH="/opt/backup"
+db1=(["hostname"]="localhost" ["username"]="root" ["password"]="" ["dbname"]="test" ["siteurl"]="xyz.com")
+db2=(["hostname"]="localhost" ["username"]="root" ["password"]="" ["dbname"]="test1" ["siteurl"]="xyz.com") 
 
-# Log variable
-LOG_FILE_NAME="backuplog.log"
+# Please insert the database variable in this array
+# Example :
+# 	db_set=(db1 db2)
+db_set=(db1 db2)
 
-# Enable backup removal (true or false)
-BK_REMOVE="true"
-# Days after sql dump should be deleted from the system
-BK_REMOVE_DAYS=10
+# Backup & Log Variable
+db_bk_path="/opt/backup"
+log_file="backuplog.log"
+
+# Enable backup removal (true or false) and 
+bk_remove="true"
+
+# Days after sql dump will be deleted from the system
+bk_remove_days=10
 
 # Create the folder if the backup location is not available
-if [ ! -d "$BK_PATH/$(date +%Y-%m-%d)" ]; then
-    mkdir -p "$BK_PATH/$(date +%Y-%m-%d)"
+if [ ! -d "$db_bk_path/$(date +%Y-%m-%d)" ]; then
+    mkdir -p "$db_bk_path/$(date +%Y-%m-%d)"
 fi
 
 # Create the log file if it is not available
-if [ ! -f "$BK_PATH/$LOG_FILE_NAME" ]; then
-    echo "" > "$BK_PATH/$LOG_FILE_NAME"
+if [ ! -f "$db_bk_path/$log_file" ]; then
+    echo "" > "$db_bk_path/$log_file"
 fi
 
 # Remove the old backup dumps
-if [ $BK_REMOVE == "true" ]; then
-    find $BK_PATH -maxdepth 1 -type d -mtime +$BK_REMOVE_DAYS -exec rm -rf {} \;
+if [ $bk_remove == "true" ]; then
+    find $db_bk_path -maxdepth 1 -type d -mtime +$bk_remove_days -exec rm -rf {} \;
 fi
 
 # Backup database on the system
-mysqldump --user=$MYSQL_USER_NAME --password=$MYSQL_USER_PWD --opt $MYSQL_DB_NAME | gzip > "$BK_PATH/`date +%Y-%m-%d`/mysqldump-$HN-$MYSQL_DB_NAME-$(date +%Y-%m-%d).sql.gz";
+for db in ${db_set[*]}
+do
+	hostname="$db["hostname"]"
+	username="$db["username"]"
+	password="$db["password"]"
+	dbname="$db["dbname"]"
+	siteurl="$db["siteurl"]"
+	#MySQLdump cmd
+	mysqldump -u ${!username} -p${!password} -h ${!hostname} ${!dbname} | gzip > "$db_bk_path/$(date +%Y-%m-%d)/mysql-${!siteurl}-${!dbname}-$(date +%Y-%m-%d).sql.gz";
+done
 
-echo "Backup completed for `date +%Y-%m-%d`" >> "$BK_PATH/$LOG_FILE_NAME"
-
-
-
-
+#completed status
+echo "Backup completed on `date +%Y-%m-%d %H:%M:%S`" >> "$db_bk_path/$log_file"
